2289f6f3ddeae4e980ddd1598ad193c64ee63a1c
Hyeonjun Park <phjppo0918@gmail.com>
2022-12-29 21:54:57 +0900

    feat : 시큐리티 설정 컴포넌트 구현

--- server/src/main/java/com/pettion/server/global/config/security/SecurityConfig.java
+++ server/src/main/java/com/pettion/server/global/config/security/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.pettion.server.global.config.security;
 
+import com.pettion.server.global.config.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,16 +26,55 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuthUserService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationSuccessHandler successHandler;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     *
+     * @author Hyeonjun Park
+     */
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+        return http.authorizeHttpRequests(requests -> requests
+                        .antMatchers("/api/auth/login/**").permitAll()
+                        .antMatchers("api/test/**").authenticated()
+                )
+                //.formLogin().disable()
+                .oauth2Login(setOAuth2Config())
+                .sessionManagement(setSessionManagementConfig())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
                 .build();
     }
+
+    /**
+     * Session management 설정
+     * JWT 방식으로 인증을 진행하기에 SessionCreation 정책을 STATELESS로 변경
+     *
+     * @author Hyeonjun Park
+     */
+    private Customizer<SessionManagementConfigurer<HttpSecurity>> setSessionManagementConfig() {
+        return s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    /**
+     * OAuth2 설정
+     *
+     * @author Hyeonjun Park
+     */
+    private Customizer<OAuth2LoginConfigurer<HttpSecurity>> setOAuth2Config() {
+        return o -> o.authorizationEndpoint(
+                        auth -> auth.baseUri("/api/auth/login"))
+                .userInfoEndpoint(e -> e.userService(oAuthUserService))
+                .successHandler(successHandler);
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
