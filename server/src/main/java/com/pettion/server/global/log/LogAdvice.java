ff04b7588e878b1cf3d37794fb3f41919d836cf7
Hyeonjun Park <phjppo0918@gmail.com>
2022-12-29 21:54:38 +0900

    fix: Spring boot 다운그레이드하면서 변경된 라이브러리 적용

--- server/src/main/java/com/pettion/server/global/log/LogAdvice.java
+++ server/src/main/java/com/pettion/server/global/log/LogAdvice.java
@@ -1,4 +1,12 @@
 package com.pettion.server.global.log;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
 public class LogAdvice {
+
 }
