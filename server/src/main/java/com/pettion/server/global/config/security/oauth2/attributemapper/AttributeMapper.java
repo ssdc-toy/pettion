c20cec3e87c17aad88b495a78b60551ade7cf1ef
Hyeonjun Park <phjppo0918@gmail.com>
2022-12-29 21:51:27 +0900

    feat : oauth2 로그인 시 소셜 통합적으로 mapping 진행하는 컴포넌트 구현

---
+++ server/src/main/java/com/pettion/server/global/config/security/oauth2/attributemapper/AttributeMapper.java
@@ -0,0 +1,30 @@
+package com.pettion.server.global.config.security.oauth2.attributemapper;
+
+import com.pettion.server.global.config.security.oauth2.OAuth2Request;
+import com.pettion.server.model.bidder.entity.AuthProvider;
+import com.pettion.server.model.bidder.entity.Bidder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AttributeMapper {
+
+    private final AttributeMapperFactory attributeMapperFactory;
+
+    public OAuth2Request mapToUser(AuthProvider provider, Map<String, Object> attributes) {
+        return attributeMapperFactory.get(provider).mapToDTO(attributes);
+    }
+
+    public Map<String, Object> mapToAttribute(Bidder user) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", user.getId());
+        result.put("accountId", user.getAccountId());
+        result.put("nickname", user.getNickname());
+        result.put("role", user.getRole());
+        return result;
+    }
+}
